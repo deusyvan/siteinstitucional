679b32a1922da74552fe78e8b1afed02258d6970
DFSWeb <contato@dfsweb.com.br>
2020-01-16 22:23:35 -0300

    Criando o corpo do email

--- Controllers/ContatoController.php
+++ Controllers/ContatoController.php
@@ -8,6 +8,15 @@ class ContatoController extends Controller{
     public function index(){
         $dados = array();
         
+        if (isset($_POST['nome']) && !empty($_POST['nome'])){
+            $nome = addslashes($_POST['nome']);
+            $email = addslashes($_POST['email']);
+            $msg = addslashes($_POST['mensagem']);
+            
+            $para = "contato@dfsweb.com.br";
+            $assunto = "Dúvida do Site";
+            $mensagem = "Nome: ".$nome."<br/>E-mail: ".$email."<br/>Mensagem: ".$msg;
+        }
         
         $this->loadTemplate('contato', $dados);
     }
